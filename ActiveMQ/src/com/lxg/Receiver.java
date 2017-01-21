ec8eb51c0e8963cf77071ed1ebb4a56a9e474503
俩天 <liuxuegang@sinoiov.com>
2017-01-21 15:36:22 +0800

    添加注释

--- ActiveMQ/src/com/lxg/Receiver.java
+++ ActiveMQ/src/com/lxg/Receiver.java
@@ -17,15 +17,28 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 public class Receiver {
 
     public static void main(String[] args) throws JMSException {
+        // Create a ConnectionFactory，创建连接工厂
         ConnectionFactory factory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD,
                 "tcp://127.0.0.1:61616");
+        // Create a Connection，创建连接
         Connection connection = factory.createConnection();
+        //打开连接
         connection.start();
-        Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createQueue("TestQueue");
 
+        // Create a Session，创建会话
+        Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);//指定ACK_Mode签收确认模式为自动确认
+
+        // Create the destination (Topic or Queue)
+        Destination destination = session.createQueue("TestQueue");//创建消息目标(点对点模型队列)
+        //Destination destination = session.createTopic("TEST.FOO");//创建消息目标(订阅主题)
+
+        // Create a MessageConsumer from the Session to the Topic or Queue//创建消息消费者
         MessageConsumer consumer = session.createConsumer(destination);
-        ObjectMessage message = (ObjectMessage)consumer.receive();
+
+        // Wait for a message
+        ObjectMessage message = (ObjectMessage)consumer.receive();//接收到达的消息，如果没有收到此方法将阻塞等待直到指定超时时间
+
+        //判断消息类型是否为空
         if (message != null) {
             String messageString = (String)message.getObject();
             System.out.println("Receive : " + messageString);
--- ActiveMQ/src/com/lxg/Sender.java
+++ ActiveMQ/src/com/lxg/Sender.java
@@ -19,15 +19,28 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 public class Sender {
 
     public static void main(String[] args) throws JMSException {
+        // Create a ConnectionFactory，创建连接工厂
         ConnectionFactory factory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_USER, ActiveMQConnection.DEFAULT_PASSWORD, "tcp://127.0.0.1:61616");
+
+        // Create a Connection，创建连接
         Connection connection = factory.createConnection();
+        connection.start();//打开连接
         connection.start();
-        Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
-        Destination destination = session.createQueue("TestQueue");
-        MessageProducer producer = session.createProducer(destination);
-        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-        ObjectMessage message = session.createObjectMessage("hello world...");
-        producer.send(message);
+
+        // Create a Session//创建会话
+        Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);//指定ACK_Mode签收确认模式为自动确认
+
+        // Create the destination (Topic or Queue)
+        Destination destination = session.createQueue("TestQueue");//创建消息目标(点对点模型队列)
+        //Destination destination = session.createTopic("TEST.FOO");//创建消息目标(订阅主题)
+
+        // Create a MessageProducer from the Session to the Topic or Queue,创建消息生产者
+        MessageProducer producer = session.createProducer(destination);//创建消息生产者
+        producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);//指定传输模式-非持久性消息
+
+        ObjectMessage message = session.createObjectMessage("hello world...");//创建消息
+        producer.send(message);//发送消息
+
         session.commit();
         System.out.println("sent...");
     }
